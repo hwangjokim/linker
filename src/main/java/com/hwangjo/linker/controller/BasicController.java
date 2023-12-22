1e41b13149d4c1ebb7448f16c3de78adcd02e41d
hwangjokim <hg7258@naver.com>
2023-12-22 23:51:53 +0900

    Add : 메인 페이지 뷰 추가 및 레이아웃 구성

---
+++ src/main/java/com/hwangjo/linker/controller/BasicController.java
@@ -0,0 +1,23 @@
+package com.hwangjo.linker.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class BasicController {
+
+    @GetMapping
+    public String mainPage(Principal principal){
+        if (principal == null)
+            return "anonymous_main";
+        return "main";
+    }
+}
